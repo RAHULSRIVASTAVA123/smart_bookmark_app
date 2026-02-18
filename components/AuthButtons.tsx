3bbcc308007d579827e548a6b9dd383649d8435a
RAHULSRIVASTAVA123 <97719882+RAHULSRIVASTAVA123@users.noreply.github.com>
2026-02-18 19:21:43 +0530

    Update AuthButtons.tsx

--- components/AuthButtons.tsx
+++ components/AuthButtons.tsx
@@ -1,24 +1,35 @@
-
 "use client";
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
 
+type Props = {
+  user: any;
+};
 
-export default function AuthButtons() {
+export default function AuthButtons({ user }: Props) {
   const loginWithGoogle = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: window.location.origin, 
+        redirectTo: window.location.origin,
       },
     });
   };
 
+  const logout = async () => {
+    await supabase.auth.signOut();
+  };
+
   return (
-    <button
-      onClick={loginWithGoogle}
-      className="px-4 py-2 rounded bg-black text-white"
-    >
-      Login with Google
-    </button>
+    <div>
+      {user ? (
+        <button onClick={logout} className="px-4 py-2 bg-red-500 text-white rounded">
+          Logout
+        </button>
+      ) : (
+        <button onClick={loginWithGoogle} className="px-4 py-2 bg-black text-white rounded">
+          Login with Google
+        </button>
+      )}
+    </div>
   );
 }
